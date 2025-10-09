b3e7cf6aaa1c5ca49609c659da5f94bd9ea2fed3
anchalshivank <anchalshivank99@gmail.com>
2025-10-09 11:23:21 +0530

    fix build issue

--- src/test/java/com/renter/auth/AuthServiceApplicationTests.java
+++ src/test/java/com/renter/auth/AuthServiceApplicationTests.java
@@ -2,9 +2,18 @@ package com.renter.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.renter.auth.service.KeycloakService;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AuthServiceApplicationTests {
+
+	@MockBean
+	private KeycloakService keycloakService;
+
 
 	@Test
 	void contextLoads() {
